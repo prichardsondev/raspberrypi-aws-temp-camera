25aedfb869c8760b90f3cd2db13b90be10f2c113
prichardsondev <prichardsondev@gmail.com>
2023-02-26 19:06:50 -0500

    initial

---
+++ api/service.js
@@ -0,0 +1,48 @@
+//preform logic -> return response to controller
+require('dotenv').config();
+
+const sensor = require("node-dht-sensor").promises;
+const libcamera = require('node-libcamera')
+
+
+const service = {
+    getTemp: async () => {
+        const res = await sensor.read(11, 4);
+
+        return {
+            temp: res.temperature.toFixed(2),
+            timestamp: Date.now(),
+            machineID: process.env.MACHINE_ID
+        };
+    },
+    getImage: async () => {
+
+        try {
+            const timestamp = Date.now();
+            let output = `${__dirname}/public/images/${timestamp}.jpg`;
+
+            const config = {
+                output, // output file path
+                timeout: 2000, // timeout before taking the picture
+                width: 640, // image width
+                height: 480, // image height
+                nopreview: true,
+            };
+
+            let pic = await libcamera.still(config);
+
+            return {
+                output: config.output,
+                timestamp,
+                machineID: process.env.MACHINE_ID
+            }
+        } catch (err) {
+            console.log('service.js err', err);
+        }
+
+    }
+};
+
+module.exports = {
+    service,
+};
